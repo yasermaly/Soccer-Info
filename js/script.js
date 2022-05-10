046d61dd5d847fb18e8630cde9dd84bfc78548fb
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-05-09 20:36:33 -0700

    add data points in html and ajax datapoint request in js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,52 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://api-football-v1.p.rapidapi.com/v3/timezone",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-		"X-RapidAPI-Key": "ac40a89d57msh539caae758e05fdp1376f5jsn7ecabd3512ad"
-	}
-};
+// //VARIABLES
+// const url = "https://example.p.rapidapi.com/?rapidapi-key=ac40a89d57msh539caae758e05fdp1376f5jsn7ecabd3512ad"
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// const $name = $('#name')
+// const $age = $('#age')
+// const $club = $('#club')
+// const $league = $('league')
+// const $goals = $('#goals')
+// const $assists = $('$assists')
+// const $form = $('form')
+// const $input = $(`input[type="text"]`)
+
+// // EVENT LISTENER
+
+// $form.on('submit', handleGetData)
+
+// // FUNCTIONS
+
+// function handleGetData(event) {
+    
+//     event.preventDefault()
+//     const userInput = $input.val()
+    
+//     $.ajax(url + userInput).then(function(data) {
+//         console.log('Player data is ready')
+//         console.log(data);
+//         $name.text(data.Name)
+//         $age.text(data.Age)
+//         $club.text(data.Club)
+//         $league.text(data.League)
+//         $goals.text(data.Goals)
+//         $assists.text(data.Assists)
+//         $main.append(`<img src="${data.Poster}"/>`)
+//     },  function (error) {
+//         console.log('something is wrong')
+//         console.log(error)
+//     });
+
+// }
+
+var settings = {
+    "url": "https://soccer.sportmonks.com/api/v2.0/players/search/neymar",
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+
+
