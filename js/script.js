1595efae6b5198b4d8e0a502efa401bd4a59011a
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-05-11 11:07:55 -0700

    API is displaying on the page! Moving over to

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
 // GLOBAL VARIABLES
 
 const apiToken = "MMYVfqsnHtw5rzbBPzmwyiRq6wIAV0bCuV99DTlXwKNJd78W3ptKvQ8n88hU"
+const $main = $('main')
 const $name = $('#name')
 const $dateOfBirth = $('#birth-date')
 const $club = $('#club')
 const $league = $('league')
 const $goals = $('#goals')
-
+const $ul = $('ul')
 const $form = $('form')
 const $input = $(`input[type="text"]`)
-const userInput = $input.val()
 
-// SOCCER INFO ATTEMPTED AJAX REQUEST
 
-const settings = {
-    "url": `"https://soccer.sportmonks.com/api/v2.0/players/search/${userInput}?api_token=${apiToken}&include="`,
-    "method": "GET",
-    "timeout": 0,
-  };
-
+// SOCCER INFO AJAX REQUEST TO RETRIEVE DATA FROM API
 
   $form.on('submit', handleGetData)
 
@@ -26,20 +20,30 @@ const settings = {
   
       event.preventDefault()
       const userInput = $input.val()
+      const userInput2 = userInput.replace(/ /g, '+')
+      const settings = {
+        "url": `https://soccer.sportmonks.com/api/v2.0/players/search/${userInput2}?api_token=${apiToken}&include=`,
+        "method": "GET",
+        "timeout": 0,
+      };
       console.log(userInput)
+      console.log(apiToken)
     
     $.ajax(settings).then(function (data) {
-      console.log(data);
-      $name.text(data[0].display_name)
+      console.log(data.data[1]);
+      $name.text(data.data[1].display_name)
+      $main.append(`<img src="${data.data[1].image_path}"/>`)
+      //const $li = $ul.append(`<li>${data[0].}</li>`)
     });
-  
+    
+  $input.val('')
   }
 
 
 // SPORTMONKS SAMPLE AJAX REQEST
 
 // var settings = {
-//     "url": "https://soccer.sportmonks.com/api/v2.0/players/search/Zlatan+Ibrahimovic?api_token=MMYVfqsnHtw5rzbBPzmwyiRq6wIAV0bCuV99DTlXwKNJd78W3ptKvQ8n88hU&include=",
+//     "url": "https://soccer.sportmonks.com/api/v2.0/players/search/Lionel+Messi?api_token=MMYVfqsnHtw5rzbBPzmwyiRq6wIAV0bCuV99DTlXwKNJd78W3ptKvQ8n88hU&include=",
 //     "method": "GET",
 //     "timeout": 0,
 //   };
